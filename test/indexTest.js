e667a34c82c32e657032375492a178897754632d
winton <mail@wintoni.us>
2018-11-13 20:21:18 -0500

    Upgrade cyclops, test

--- test/indexTest.js
+++ test/indexTest.js
@@ -1,5 +1,62 @@
-import * as lib from "../dist/git"
+import cyclops from "cyclops"
+import dotEvent from "dot-event"
+import dotStore from "dot-store"
+import git from "../dist/git"
 
-test("export something", () => {
-  expect(Object.keys(lib).length).toBeGreaterThan(0)
+let events, store
+
+beforeEach(async () => {
+  events = dotEvent()
+  store = dotStore(events)
+
+  cyclops({ events, store })
+
+  events.onAny({
+    "before.spawn": ({ event }) => {
+      event.signal.cancel = true
+    },
+  })
+})
+
+async function run(...argv) {
+  await events.cyclops({
+    argv,
+    composer: git,
+    path: `${__dirname}/fixture`,
+    task: "git-tasks",
+  })
+}
+
+test("commit", async () => {
+  const args = []
+
+  events.onAny({
+    "before.spawn": ({ event }) => args.push(event.args[0]),
+  })
+
+  await run("-c", "-a", "-m", "hi")
+
+  expect(args).toEqual([
+    {
+      args: ["add", "."],
+      command: "git",
+      options: {
+        cwd: `${__dirname}/fixture/project-a`,
+      },
+    },
+    {
+      args: ["add", ".", "-u"],
+      command: "git",
+      options: {
+        cwd: `${__dirname}/fixture/project-a`,
+      },
+    },
+    {
+      args: ["commit", "-a", "-m", "hi"],
+      command: "git",
+      options: {
+        cwd: `${__dirname}/fixture/project-a`,
+      },
+    },
+  ])
 })
